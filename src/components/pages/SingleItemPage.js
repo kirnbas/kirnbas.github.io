873c602fe1281c1b43f42cf73a360f59d42217a1
kirnbas <kirnbas@gmail.com>
2024-02-07 19:31:31 +0600

    Added React Helmet

--- src/components/pages/SingleItemPage.js
+++ src/components/pages/SingleItemPage.js
@@ -1,5 +1,6 @@
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Helmet } from "react-helmet";
 
 import useMarvelService from '../../services/MarvelService';
 import AppBanner from '../appBanner/AppBanner';
@@ -51,6 +52,13 @@ const ComicView = ({comic}) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
@@ -70,6 +78,13 @@ const CharView = ({char}) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} character`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__charimg"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
